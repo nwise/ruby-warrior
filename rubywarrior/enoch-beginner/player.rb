88c9f019f9fee7d72e0c2cbd5436c65ea1b21c91
Nathan Wise <nathanwise@gmail.com>
2012-06-19 08:08:57 -0400

    LVL 6

--- rubywarrior/enoch-beginner/player.rb
+++ rubywarrior/enoch-beginner/player.rb
@@ -1,5 +1,72 @@
 class Player
+  attr_reader :warrior
+
   def play_turn(warrior)
-    # add your code here
+    @warrior = warrior
+    if can_rescue?
+      take_action(:rescue!, @rescue_direction)
+      return
+    elsif danger_ahead?
+      take_action(:walk!, :backward)
+      return
+    elsif facing_enemy?
+      take_action(:attack!, :forward)
+      return
+    elsif hurt?
+      take_action(:rest!)
+      return
+    else
+      take_action(:walk!)
+      return
+    end
+
   end
+
+  def can_rescue?
+    @rescue_direction = :forward
+    can_rescue = false
+    [:forward, :backward].each do |direction|
+      if warrior.feel(direction).captive?
+        @rescue_direction = direction
+        can_rescue = true
+      end
+    end
+    return can_rescue
+  end
+
+  def danger_ahead?
+    been_attacked? and !facing_enemy?
+  end
+
+  def facing_enemy?
+    warrior.feel.enemy?
+  end
+
+  def been_attacked?
+    @health.to_i > warrior.health
+  end
+
+  def hurt?
+    warrior.health < 20
+  end
+
+  def take_action(action, params=:forward)
+    @health = warrior.health
+    @last_action = action
+    case action
+    when :walk!
+      warrior.walk!(params)
+    when :attack!
+      warrior.attack!(params)
+    when :rest!
+      warrior.rest!
+    when :shoot!
+      warrior.shoot!(params)
+    when :rescue!
+      warrior.rescue!(params)
+    else
+      raise "No action taken"
+    end
+  end
+
 end
